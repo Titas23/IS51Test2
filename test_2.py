1a4329a4cc914564f0b2480da59abf1810d72d8c
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-05-06 15:11:45 -0700

    pseudo-code done

--- test_2.py
+++ test_2.py
@@ -12,3 +12,24 @@ We will begin with a main() function to start the program.
 
 """
 
+"""
+main()
+    infile = open Final.txt
+    grades = line.rstrip()
+    infile.close()
+    for i in range(len(grades)):
+
+calculate_percent_above_average():
+    average = sum grades/ len
+    num = 0 
+    for grade in grades:
+        if grade > average: 
+            num += 1
+print(Number of grades )
+print(Average Grade )
+print(Percentange of grades above average )
+
+main()
+
+
+"""
